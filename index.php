1421361056783d1dcd6b3345193f23ffc0e5c69c
micki444 <96817819+micki444@users.noreply.github.com>
2023-12-08 16:51:45 -0600

    Actualizacion index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 session_start();
-
 header('Content-Type: text/html; charset=UTF-8');
-
 include_once 'modules/conexion.php';
 include_once 'modules/cookie.php';
-
-
 if (!empty($_SESSION['authenticate']) == 'go-' . !empty($_SESSION['usuario'])) {
 	header('Location: home');
 	exit();
@@ -14,24 +10,24 @@ if (!empty($_SESSION['authenticate']) == 'go-' . !empty($_SESSION['usuario'])) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1" />
-	<meta name="google" value="notranslate">
+	<link rel="manifest" href="./manifest.json"/>
 	<link rel="icon" type="image/png" href="/images/icon.png" />
+	<script src="/app.js"></script>
 	<title>Sistema Escolar</title>
 	<meta name="description" content="Sistema Escolar, gestión de asistencias." />
-	<meta name="keywords" content="Sistema Escolar, Asistencias, Alumnos, Docentes, Administrativos, Sistema de Asistencias, MySoftUP, Diego, Carmona, Bernal, Diego Carmona Bernal, Gestión de Asistencias" />
+	<meta name="keywords" content="Sistema Escolar, Asistencias, Alumnos, Docentes, Administrativos, Sistema de Asistencias, mickiDev, Miguel Angel, Rodriguez, Flores, Miguel Angel Rodriguez Flores, Gestión de Asistencias" />
 	<link rel="stylesheet" href="/css/style.css?v=<?php echo(rand()); ?>" media="screen, projection" type="text/css" />
 	<link rel="stylesheet" href="css/pretty-checkbox.css" media="screen, projection" type="text/css" />
 	<script src="/js/external/jquery.min.js" type="text/javascript"></script>
-    <script src="/js/external/prefixfree.min.js" type="text/javascript"></script>
+	<script src="/js/external/prefixfree.min.js" type="text/javascript"></script>
 	<script type="text/javascript">
 		$(window).load(function() {
 			$(".loader").fadeOut("slow");
 		});
-	</script>
+	</script>	
 </head>
 
 <body class="login">
@@ -40,14 +36,23 @@ if (!empty($_SESSION['authenticate']) == 'go-' . !empty($_SESSION['usuario'])) {
 		<div class="title-login">
 			<h1>Sistema Escolar</h1>
 		</div>
+		<div id="google_translate_element" class="google">
+			<script type="text/javascript">
+				function googleTranslateElementInit() {
+					new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,eu,gl,en,fr,it,pt,de,es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true}, 'google_translate_element');
+					</script>
+		</div>
 	</div>
 	<div class="form-login">
 		<div class="logo-form-login">
 		</div>
+
 		<form name="form-login" action="" method="POST" autocapitalize="off" data-nosnippet>
 			<?php
 			include_once 'modules/login/logger.php';
 			?>
+			
+<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 		</form>
 	</div>
 </body>
